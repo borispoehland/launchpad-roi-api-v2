12b40b1baabf355d52eea9218125a902692a4220
Boris Pöhland <boris.poehland@gmail.com>
2021-11-30 13:28:11 +0100

    added IGOs held

--- src/common-logic/index.js
+++ src/common-logic/index.js
@@ -65,36 +65,7 @@ const fetchCoinData = (coins) => {
     }
 }
 
-const parseOverviewData = (data) => {
-    const filteredData = data.filter((coin) => coin.onCoinGecko)
-    const sortedData = filteredData.sort((a, b) => b.athROI - a.athROI)
-
-    const highestXCoin = sortedData[0]
-    const lowestXCoin = sortedData[sortedData.length - 1]
-
-    const avgXAllCoins = trimNumber(
-        sortedData.reduce((a, b) => a + b.athROI, 0) / sortedData.length
-    )
-
-    const avgX = {
-        label: 'Average ROI',
-        value: avgXAllCoins,
-    }
-
-    const highestX = {
-        label: `Maximum ROI (${highestXCoin.name})`,
-        value: highestXCoin.athROI,
-    }
-
-    const lowestX = {
-        label: `Minimum ROI (${lowestXCoin.name})`,
-        value: lowestXCoin.athROI,
-    }
-
-    return [highestX, avgX, lowestX]
-}
-
 module.exports = {
     fetchCoinData,
-    parseOverviewData,
+    trimNumber,
 }
--- src/launchpads/overview.js
+++ src/launchpads/overview.js
@@ -1,4 +1,38 @@
-const { fetchCoinData, parseOverviewData } = require('../common-logic')
+const { fetchCoinData, trimNumber } = require('../common-logic')
+
+const parseOverviewData = (data) => {
+    const filteredData = data.filter((coin) => coin.onCoinGecko)
+    const sortedData = filteredData.sort((a, b) => b.athROI - a.athROI)
+
+    const highestXCoin = sortedData[0]
+    const lowestXCoin = sortedData[sortedData.length - 1]
+
+    const avgXAllCoins = trimNumber(
+        sortedData.reduce((a, b) => a + b.athROI, 0) / sortedData.length
+    )
+
+    const avgX = {
+        label: 'Average ROI',
+        value: avgXAllCoins,
+    }
+
+    const highestX = {
+        label: `Maximum ROI (${highestXCoin.name})`,
+        value: highestXCoin.athROI,
+    }
+
+    const lowestX = {
+        label: `Minimum ROI (${lowestXCoin.name})`,
+        value: lowestXCoin.athROI,
+    }
+
+    const igosHeld = {
+        label: 'IGOs held',
+        value: data.length,
+    }
+
+    return [highestX, avgX, lowestX, igosHeld]
+}
 
 module.exports = async function (launchpad) {
     const coins = require(`./${launchpad}/coins.json`)
