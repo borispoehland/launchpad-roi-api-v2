b0ef1bd574b80a471b320f764b97fbcc1b4638f8
Boris Pöhland <boris.poehland@gmail.com>
2022-01-01 12:21:56 +0100

    added current ROI

--- src/launchpads/overview.js
+++ src/launchpads/overview.js
@@ -1,40 +1,50 @@
 const { fetchCoinData, trimNumber } = require('../common-logic')
 
-const parseOverviewData = (data) => {
-    const filteredData = data.filter((coin) => coin.onCoinGecko && coin.ath)
-    const sortedData = filteredData.sort((a, b) => b.athROI - a.athROI)
+const parseMinMaxAvg = (data, priceField, roiField, labelChunk) => {
+    const filteredData = data.filter(
+        (coin) => coin.onCoinGecko && coin[priceField]
+    )
+    const sortedData = filteredData.sort((a, b) => b[roiField] - a[roiField])
 
     const highestXCoin = sortedData[0]
     const lowestXCoin = sortedData[sortedData.length - 1]
 
     const avgXAllCoins = trimNumber(
-        sortedData.reduce((a, b) => a + b.athROI, 0) / sortedData.length
+        sortedData.reduce((a, b) => a + b[roiField], 0) / sortedData.length
     )
 
     const avgX = {
-        label: 'Average ROI',
+        label: `Average ${labelChunk} ROI`,
         value: avgXAllCoins,
         suffix: 'x',
     }
 
     const highestX = {
-        label: `Maximum ROI (${highestXCoin.name})`,
-        value: highestXCoin.athROI,
+        label: `Max ${labelChunk} ROI (${highestXCoin.name})`,
+        value: highestXCoin[roiField],
         suffix: 'x',
     }
 
     const lowestX = {
-        label: `Minimum ROI (${lowestXCoin.name})`,
-        value: lowestXCoin.athROI,
+        label: `Min ${labelChunk} ROI (${lowestXCoin.name})`,
+        value: lowestXCoin[roiField],
         suffix: 'x',
     }
 
+    return [highestX, avgX, lowestX]
+}
+
+const parseOverviewData = (data) => {
     const igosHeld = {
         label: 'IGOs held',
         value: data.length,
     }
 
-    return [highestX, avgX, lowestX, igosHeld]
+    return [
+        ...parseMinMaxAvg(data, 'ath', 'athROI', 'ATH'),
+        ...parseMinMaxAvg(data, 'current', 'currentROI', 'current'),
+        igosHeld,
+    ]
 }
 
 module.exports = async function (launchpad) {
